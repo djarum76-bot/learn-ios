28a294f20f1c206419aa25219a76b7186ad3db9c
Habil <habil0105@gmail.com>
2023-12-24 22:45:03 +0700

    feat: adding new project with core image for editing image as the main feature

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by habil . on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
